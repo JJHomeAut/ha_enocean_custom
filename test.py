b72cb869643b17f6fcb6f4f3932e259b91d9f235
JJHomeAut <janjenke90@googlemail.com>
2025-11-15 19:22:42 +0000

    Remove test.py file containing unused packet building code

--- test.py
+++
@@ -1,9 +0,0 @@
-from enocean_library.protocol.packet import Packet
-
-# Read base id from EnOcean dongle and build a packet
-packet_type = 0x01
-data = [0xD2, 0x01, 1, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00]
-optional = []
-packet = Packet(packet_type, data=data, optional=optional)
-packet.build()  # This will create the byte representation of the packet
-print("Packet built:", packet)
